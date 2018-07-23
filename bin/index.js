521791c58472dac22f0f4135ad050519c3701bca
amir <ahsprim@gmail.com>
2018-07-24 00:33:03 +0430

    remove apk

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
 const path = require('path');
 const fs = require('fs');
-const { install, update, restart, flushLogs, stopLog, liveLog } = require('../lib/build');
-const { connect, connectOverWIFI, connectOverWIFIWithIP } = require('../lib/connect');
 const { logger } = require('../lib/utils');
+const commandHandler = require('../lib/main/command');
 const config = require('../lib/config');
 let helpMessage = 'Available Commands:\n' +
   ' i, install\t remove the package from device and install it again\n' +
@@ -14,153 +13,19 @@ let helpMessage = 'Available Commands:\n' +
   ' ew, end watch\t end watch mode\n' +
   ' cw, connect wifi\t connect to the device only over wifi\n' +
   ' f, flush\t flush all logs\n' +
-  ' ll\t monitor logs\n';
+  ' ll\t monitor logs\n' +
+  ' sl, stop live logging\n';
 
-run();
-
-function run() {
+(() => {
 
   global.loggingLevel = 'info';
 
   setup();
 
-  if (!global.packageName) {
-    console.log('\x1b[31m%s\x1b[0m', 'Package Name not Specified');
-    process.exit();
-  }
-  if (!global.packagePath) {
-    console.log('\x1b[31m%s\x1b[0m', 'Package .apk File Path not Specified');
-    process.exit();
-  }
-  if (global.packagePath[0] !== '/') {
-    global.packageAbsolutePath = path.join(process.cwd(), global.packagePath);
-  }
-  if (!global.packageAbsolutePath.includes('.apk')) {
-    console.log('\x1b[33m%s\x1b[0m', 'Warning: Package File is not .apk');
-  }
-  if (!fs.existsSync(global.packageAbsolutePath)) {
-    console.log('\x1b[33m%s\x1b[0m', 'Warning: Package File not Exists at Specified Path');
-  }
+  configure();
 
-
-  logger.success('Debugger Started');
-  logger.info(`Configuration:\n`
-    + ` Package Name:\t${global.packageName}\n`
-    + ` Package Path:\t${global.packageAbsolutePath}\n`
-    + ` Using IP:\t${global.deviceIP ? global.deviceIP : 'No'}\n`
-    + ` Server Port:\t${global.ADBPort ? global.ADBPort : 'Default'}\n`);
-  fs.watchFile(global.packageAbsolutePath, {}, async(event) => {
-    if (watching) {
-      try {
-        await install();
-        await restart();
-      } catch (error) {
-        logger.error(error.message);
-        logger.hint(error.hint);
-      }
-    } else {
-    }
-  });
-  handleInputs();
-}
-let watching = false;
-
-async function watch() {
-  return new Promise(async (resolve,reject) => {
-    try {
-      watching = true;
-      await install();
-      await restart();
-      await liveLog('SystemWebChromeClient');
-    } catch (error) {
-      watching = false;
-      logger.error(error.message);
-      logger.hint(error.hint);
-    }
-  });
-}
-async function endWatch() {
-  return new Promise(async(resolve, reject) => {
-    watching = false;
-    await stopLog();
-    resolve()
-  });
-}
-
-function handleInputs() {
-  process.stdin.on('data', async function(data) {
-    return new Promise(async(resolve, reject) => {
-      let command = data.toString().trim();
-      try {
-        switch (command) {
-          case 'install':
-          case 'i':
-            await install();
-            break;
-          case 'update':
-          case 'u':
-            await update();
-            break;
-          case 'restart':
-          case 'r':
-            await restart();
-            break;
-          case 'watch':
-          case 'w':
-            await watch();
-            break;
-          case 'end watch':
-          case 'ew':
-            await endWatch();
-            break;
-          case 'connect':
-          case 'c':
-            await connect();
-            break;
-          case 'connect wireless':
-          case 'cw':
-            if (global.deviceIP)
-              await connectOverWIFIWithIP(global.deviceIP);
-            else
-              await connectOverWIFI();
-            break;
-          case 'help':
-            console.log(helpMessage);
-            break;
-          case 'exit':
-            process.exit();
-            break;
-          case 'f':
-          case 'flush':
-            await flushLogs();
-            logger.success('Log file flushed');
-            logger.flush();
-            break;
-          case 'ir':
-            await install();
-            await restart();
-            break;
-          case 'sl':
-            await stopLog();
-            break;
-          default:
-            let words = command.split(' ');
-            if (words[0] === 'll' || words[0] === 'live log') {
-              await liveLog(words[1] || 'SystemWebChromeClient');
-            } else {
-              console.log(`${command}: Command not Found`);
-            }
-            break;
-        }
-      } catch (error) {
-        logger.error(error.message);
-        logger.hint(error.hint);
-      }
-    });
-  }).on('end', function() {
-    console.log('Done');
-  });
-}
+  commandHandler();
+})();
 
 function setup() {
   let noIP = true;
@@ -199,4 +64,31 @@ function setup() {
     console.log('\x1b[31m%s\x1b[0m', 'Config File not Specified');
     process.exit();
   }
+}
+
+function configure() {
+  if (!global.packageName) {
+    logger.error('Package Name not Specified');
+    process.exit();
+  }
+  if (!global.packagePath) {
+    logger.error('Package .apk File Path not Specified');
+    process.exit();
+  }
+  if (global.packagePath[0] !== '/') {
+    global.packageAbsolutePath = path.join(process.cwd(), global.packagePath);
+  }
+  if (!global.packageAbsolutePath.includes('.apk')) {
+    console.log('\x1b[33m%s\x1b[0m', 'Warning: Package File is not .apk');
+  }
+  if (!fs.existsSync(global.packageAbsolutePath)) {
+    console.log('\x1b[33m%s\x1b[0m', 'Warning: Package File not Exists at Specified Path');
+  }
+
+  logger.success('Debugger Started');
+  logger.info(`Configuration:\n`
+    + ` Package Name:\t${global.packageName}\n`
+    + ` Package Path:\t${global.packageAbsolutePath}\n`
+    + ` Using IP:\t${global.deviceIP ? global.deviceIP : 'No'}\n`
+    + ` Server Port:\t${global.ADBPort ? global.ADBPort : 'Default'}\n`);
 }
